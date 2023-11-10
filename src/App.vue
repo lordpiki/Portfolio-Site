f57b5b1da777d83b1801f8f4a78539dded0b9899
mikid <Mikidov2510@>
2023-11-10 22:01:49 +0200

    Changed name to index

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PageIndex/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageIndex from './components/PageIndex.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PageIndex
   }
 }
 </script>
@@ -20,7 +19,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #cbcbcb;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/PageIndex.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="header">
+        <h1>Hello, I'm Miki</h1>
+        <p>Welcome to my portfolio</p>
+      </div>
+
+      <!-- Projects Showcase -->
+      <div class="projects">
+        <div v-for="project in projects" :key="project.id" class="project-card">
+          <h3>{{ project.name }}</h3>
+          <p>{{ project.description }}</p>
+          <!-- Add more project details as needed -->
+        </div>
+      </div>
+
+      <!-- Contact Section -->
+      <div class="contact">
+        <h2>Contact Me</h2>
+        <p>Email: <a href="mailto:your.email@example.com">your.email@example.com</a></p>
+        <p>LinkedIn: <a href="https://www.linkedin.com/in/your-linkedin-profile/">LinkedIn Profile</a></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        { id: 1, name: 'Project 1', description: 'Description 1' },
+        { id: 2, name: 'Project 2', description: 'Description 2' },
+        // Add more projects
+      ],
+    };
+  },
+};
+</script>
+
+<style src="../styles/PageIndex.css"></style>
